47a86289aaea02dacf9224b6b3fc960ad18b3cd6
hyysb <574477988@qq.com>
2020-03-10 17:39:52 +0800

    添加滑动窗口
    Signed-off-by: hyysb <574477988@qq.com>

--- public.js
+++ public.js
@@ -1,3 +1,33 @@
+// 控制iframe canvas、代码演示窗口变化
+const iframeWidth = document.documentElement.offsetWidth;
+if(iframeWidth <= 809) {
+    document.getElementById("canvas").style.zoom = iframeWidth/809 - 0.1;
+    document.getElementById("canvas1").style.zoom = iframeWidth/809 - 0.1;
+    document.getElementsByTagName("pre")[0].style.zoom = iframeWidth/809 - 0.1;
+} else if(iframeWidth > 809) {
+    document.getElementById("canvas").style.zoom = 1;
+    document.getElementById("canvas1").style.zoom = 1;
+    document.getElementsByTagName("pre")[0].style.zoom = 1;
+}
+window.onresize = throttle(resize, 100);
+
+function resize() {
+    const iframeWidth = document.documentElement.offsetWidth;
+    if(iframeWidth <= 900 && iframeWidth > 414) {
+        document.getElementById("canvas").style.zoom = iframeWidth/809 - 0.1;
+        document.getElementById("canvas1").style.zoom = iframeWidth/809 - 0.1;
+        document.getElementsByTagName("pre")[0].style.zoom = iframeWidth/809 - 0.1;
+    }
+}
+
+// 定时绘制
+function timeout(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+// 开源节流
 function throttle(func, wait, options) {
     var timeout, context, args, result;
     var previous = 0;
